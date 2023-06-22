edf12091be29164b92cdcc707a98b6b7d83053b1
Kamente <justinm48000@gmail.com>
2023-06-22 18:24:03 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const createTaskForm = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  function createTaskItem(description) {
+
+    const taskItem = document.createElement('li');
+    const taskText = document.createElement('span');
+    const deleteButton = document.createElement('button');
+
+    taskText.innerText = description;
+
+    deleteButton.innerText = 'Remove';
+    deleteButton.addEventListener('click', function () {
+      taskList.removeChild(taskItem);
+    });
+
+
+    taskItem.appendChild(taskText);
+    taskItem.appendChild(deleteButton);
+
+    taskList.appendChild(taskItem);
+  }
+
+  createTaskForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+
+    const taskDescription = document.getElementById('new-task-description').value;
+
+    createTaskItem(taskDescription);
+
+    document.getElementById('new-task-description').value = '';
+  });
 });
